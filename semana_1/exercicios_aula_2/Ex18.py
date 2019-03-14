3df84bd90e8f95124868bfc813dfb9786812171f
Natalie Costa <natalie.dacosta@gastecnologia.com.br>
2019-03-14 16:18:18 -0300

    Fazer

---
+++ semana_1/exercicios_aula_2/Ex18.py
@@ -0,0 +1,12 @@
+'''
+Problema #18
+Usando as funções criadas nos exercícios 13, 14 e 15.
+Crie uma composição de funções que resolva os 3 casos:
+EX:
+f(x) -> Union[ID, goiabada, queijo, romeu e julieta]
+f(3) -> ‘queijo’
+f(5) -> ‘goiabada’
+f(15) -> ‘romeu e julieta’
+f(19) -> 19
+'''
+
