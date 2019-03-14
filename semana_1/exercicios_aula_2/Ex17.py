a8073f5f5c5e30c927aea8142a5f58e017af2482
Natalie Costa <natalie.dacosta@gastecnologia.com.br>
2019-03-14 16:17:16 -0300

    Fazer

---
+++ semana_1/exercicios_aula_2/Ex17.py
@@ -0,0 +1,10 @@
+'''
+Problema #17
+Faça uma função que receba uma função e uma lista de números, aplique essa
+função a todos os valores da lista e retorne uma nova lista com os valores
+processados:
+EX:
+aplica(func, list) -> [func(list[0]), func(list[n]), ..]
+aplica(soma_1, [1, 2, 3]) -> [2, 3, 4]
+aplica(sub_1, [1, 2, 3]) -> [0, 1, 2]
+'''
