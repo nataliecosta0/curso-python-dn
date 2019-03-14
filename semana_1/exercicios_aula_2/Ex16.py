ee8c26fef5977330b851f66afac4f0227ef04174
Natalie Costa <natalie.dacosta@gastecnologia.com.br>
2019-03-14 16:16:21 -0300

    corrigindo

---
+++ semana_1/exercicios_aula_2/Ex16.py
@@ -0,0 +1,23 @@
+'''
+Problema #16
+Faça uma função que receba uma função e um número. Ela deve retornar uma
+lista com todos os números em ordem crescente até aquele valor:
+EX:
+aplica_em_lote(soma_1, 3) -> [1, 2, 3]
+DICA:
+O index inicia em 0
+'''
+
+"def soma_1(numeros):
+
+
+def aplicacao_em_lote(val, numeros):
+    lista = []
+    for elemento in range(numeros):
+        lista.append(val(elemento))
+    return lista
+
+#print(aplicacao_em_lote(soma_1, 3))
+
+print(aplicacao_em_lote(val, 3))
+
