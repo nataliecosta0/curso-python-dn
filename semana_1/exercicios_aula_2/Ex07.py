75d3cf4365e88576d9926bbe81c7c4df04a7b792
Natalie Costa <natalie.dacosta@gastecnologia.com.br>
2019-03-14 12:03:17 -0300

    corrigindo

--- semana_1/exercicios_aula_2/Ex07.py
+++ semana_1/exercicios_aula_2/Ex07.py
@@ -1,5 +1,18 @@
+'''
+Problema #7
+Faça um programa que faça a inversão de uma lista usando as propriedade
+mutáveis dessa lista (remova da lista e insira de novo)
+Ex:
+Entrada = [1, 2, 3]
+Saída = [3, 2, 1]
+Coisas que podem te ajudar:
+list.insert, list.append, list.remove
+'''
+
 listaEntrada = [1, 2, 3]
+listaSaida = []
 
-listaEntrada.append(-1(listaEntrada))
+for i in listaEntrada:
+    listaSaida.insert(0, i)
 
-print(listaEntrada)
+print(listaSaida)
