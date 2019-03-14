1de141bb880130bbf3047722991eec0583ef8a40
Natalie Costa <natalie.dacosta@gastecnologia.com.br>
2019-03-14 14:37:10 -0300

    Corrigindo

--- semana_1/exercicios_aula_2/Ex15.py
+++ semana_1/exercicios_aula_2/Ex15.py
@@ -1,3 +1,11 @@
+'''
+Problema #15
+Faça uma função que se aplique uma função duas vezes em um valor passado
+EX:
+reaplica(soma_2, 2) -> 6
+reaplica(sub_2, 2) -> -2
+'''
+
 def soma_2(n):
     return n + 2
 
@@ -6,3 +14,5 @@ def sub(x, y):
 
 def aplicacao(op, val):
     return op(op(val))
+
+
