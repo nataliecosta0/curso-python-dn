a0243c5619abfc2ee6e8759f10339b80d7a2b783
Natalie Costa <natalie.dacosta@gastecnologia.com.br>
2019-03-18 14:47:25 -0300

    Atualizado

--- semana_2/exercicios_aula_3/Ex02.py
+++ semana_2/exercicios_aula_3/Ex02.py
@@ -7,10 +7,7 @@ Faça assertivas de valores que você acredita serem coerentes com a função
 def soma(x, y):
     return x + y
 
-assert(1, 1) == 2
-assert(1, 2) == 3
-assert(1, 4) == 5
-assert(1, 7) == 8
-assert(7, 7) == 14
-
-print(soma(1, 2))
+assert soma(1, 1) == 2
+assert soma(1, 2) == 3
+assert soma(1, 4) == 5
+assert soma(1, 7) == 8
