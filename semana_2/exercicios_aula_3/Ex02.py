a14a529c2299b99e463311af4b2de3a799afaaf4
Natalie Costa <natalie.dacosta@gastecnologia.com.br>
2019-03-18 14:34:31 -0300

    Aula 3

---
+++ semana_2/exercicios_aula_3/Ex02.py
@@ -0,0 +1,16 @@
+'''
+Problema #2
+Dada uma função de soma:
+Faça assertivas de valores que você acredita serem coerentes com a função
+'''
+
+def soma(x, y):
+    return x + y
+
+assert(1, 1) == 2
+assert(1, 2) == 3
+assert(1, 4) == 5
+assert(1, 7) == 8
+assert(7, 7) == 14
+
+print(soma(1, 2))
