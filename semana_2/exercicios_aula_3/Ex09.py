87b338b832a9e2f1d888951ed242362891699287
Natalie Costa <natalie.dacosta@gastecnologia.com.br>
2019-03-19 16:28:36 -0300

    atualização

--- semana_2/exercicios_aula_3/Ex09.py
+++ semana_2/exercicios_aula_3/Ex09.py
@@ -20,10 +20,10 @@ def exp(x, y, z):
 class TesteExpre(TestCase):
     dummy = 2
     def teste_soma(self):
-        self.assertEqual(exp(self.dummy, self.dummy, 2), 2)
+        self.assertEqual(exp(self.dummy, self.dummy, 2), 2) #subs de soma
 
     def teste_soma2(self):
-        self.assertEqual(exp(self.dummy, 1, 2), 1)
+        self.assertEqual(exp(self.dummy, 1, 2), 1) #uma soma
 
     def teste_soma3(self):
-        self.assertEqual(exp(0, 2, self.dummy), 0)
+        self.assertEqual(exp(0, 2, self.dummy), 0) #sub
