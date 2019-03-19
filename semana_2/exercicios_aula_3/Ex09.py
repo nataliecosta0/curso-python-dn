1bed66fcfda9e0cd0d4d66fe0eb8cba6563a216d
Natalie Costa <natalie.dacosta@gastecnologia.com.br>
2019-03-19 16:26:03 -0300

    Nova atualização

--- semana_2/exercicios_aula_3/Ex09.py
+++ semana_2/exercicios_aula_3/Ex09.py
@@ -4,6 +4,9 @@ Usando a função ‘exp’ criada no problema 7. Teste a validação indireta d
 de soma usando um dummy
 '''
 from unittest import TestCase, main 
+from typing import NewType, Any
+
+Dummy = NewType('Dummy', Any)
 
 def soma(x, y):
     return x + y
@@ -15,6 +18,12 @@ def exp(x, y, z):
     return sub(soma(x,y),z)
 
 class TesteExpre(TestCase):
-    dummy = 0
+    dummy = 2
     def teste_soma(self):
-        self.assertEqual(exp(self.dummy, self.dummy, 4), -4)
+        self.assertEqual(exp(self.dummy, self.dummy, 2), 2)
+
+    def teste_soma2(self):
+        self.assertEqual(exp(self.dummy, 1, 2), 1)
+
+    def teste_soma3(self):
+        self.assertEqual(exp(0, 2, self.dummy), 0)
