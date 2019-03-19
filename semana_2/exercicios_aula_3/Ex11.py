5a8ca9e30af91f6b78327cfe36205c952bfda66d
Natalie Costa <costa.natalie02@gmail.com>
2019-03-18 21:56:32 -0300

    Corrigir

--- semana_2/exercicios_aula_3/Ex11.py
+++ semana_2/exercicios_aula_3/Ex11.py
@@ -4,7 +4,7 @@ Usando a função ‘exp’ criada no problema 8. Faça a checagem dos inputs
 indiretos da função de soma em um teste. Em outro teste faça a função dos inputs
 indiretos da função de sub.
 '''
-
+ 
 from unittest import mock, TestCase
 
 class TestCalc(TestCase):
