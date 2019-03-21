3afe3743f5501f510c08e8d661a9e62b4aa3b3f1
Natalie Costa <natalie.dacosta@gastecnologia.com.br>
2019-03-21 16:51:16 -0300

    corrigir

--- semana_2/exercicios_aula_4/teste_ex_2.py
+++ semana_2/exercicios_aula_4/teste_ex_2.py
@@ -2,3 +2,46 @@
 Problema #2
 Me guiem usando TDD para desenvolvermos esse cenário
 '''
+
+from unittest import TestCase
+
+'''
+def sub(x, y):
+    try:
+        return x - y
+    except TypeError:
+        return 'Não é um número'
+
+def soma(x, y):
+    try:
+        return x + y
+    except TypeError:
+        return 'Não é um número'
+'''
+
+def exp(x, y, z):
+    return sub(soma(x, y), z)
+
+class TestSoma(TestCase):
+    def test_soma_deve_retornar_2_com_1_1(self):
+        self.assertEqual(soma(1, 1), 2)
+    
+    def test_soma_deve_retornar_11_com_1_1(self):
+        self.assertEqual(soma('1', '1'), '11')
+        
+    def test_soma_deve_retornar_nao_e_numero_com_4_e_eduardo(self):
+        self.assertEqual(soma(4, 'eduardo'), 'Não é um número')
+
+class Testsub(TestCase):
+    def test_sub_deve_retornar_0_com_1_1(self):
+        self.assertEqual(sub(1, 1), 0)
+
+    def test_sub_deve_retornar_0_com_1__1(self):
+        self.assertEqual(sub(1, -1), 2)
+
+class TestExp(TestCase):
+    def test_exp(self):
+        self.assertEqual(exp(1, 1, 1), 1)
+
+    def test_exp_2(self):
+        self.assertEqual(exp(lambda x: x + 1, [], 'eduardo'), None)
