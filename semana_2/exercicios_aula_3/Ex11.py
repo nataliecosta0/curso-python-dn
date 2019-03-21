8005725d68d550065ac63adeaab26f398e40f04c
Natalie Costa <natalie.dacosta@gastecnologia.com.br>
2019-03-21 16:37:29 -0300

    Corrigido

--- semana_2/exercicios_aula_3/Ex11.py
+++ semana_2/exercicios_aula_3/Ex11.py
@@ -7,9 +7,28 @@ indiretos da função de sub.
  
 from unittest import mock, TestCase
 
-class TestCalc(TestCase):
-    def test_input_indireto_soma(self):
-        with mock.patch('calc.add') as spy:
-            exp(1, 2, 3)
+def soma(x, y):
+    return x + y
 
-        spy.assert_called_with(1, 2)
+def sub(x, y):
+    return x - y
+
+def exp(x, y, z):
+    return sub(soma(x, y), z)
+
+class TestarExp(TestCase):
+    def test_exp_entrada_indireta_soma_recebe_xy(self):
+        x = 2
+        y = 3
+        z = 4
+        with mock.patch('Ex11.soma') as moked_soma:
+            exp(x, y, z)
+
+        moked_soma.assert_called_with(x, y)
+
+    def test_exp_entrada_indireta_sub_recebe_z(self):
+        with mock.patch('Ex11.sub') as moked_sub:
+            exp(2, 3, 4)
+        
+        moked_sub.assert_called_with(5, 4)
+
