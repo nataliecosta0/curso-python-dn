b95d2f2a82e80fcf0a0fb42347d0324203e49cf8
Natalie Costa <natalie.dacosta@gastecnologia.com.br>
2019-03-25 17:01:27 -0300

    Ate o 5

--- semana_2/exercicios_aula_4/teste_ex_6.py
+++ semana_2/exercicios_aula_4/teste_ex_6.py
@@ -7,3 +7,26 @@ valores:
 Faça uma função ‘filter_users’ onde serão retornados apenas os usuários com id
 superior a 100.
 '''
+
+from unittest import TestCase
+
+lista_usuario = []
+
+def get_users(lista_usuario):
+    lista_usuarios = [(10, 'Matheus', 'matheus_ss@hotmail.com'),
+                      (20, 'Lucas', 'lucas_ribeiro@hotmail.com'),
+                      (140, 'Júlia', 'julia_sensacao@hotmail.com'),
+                      (235, 'Marina', 'marina_s2@hotmail.com'),
+                      (80, 'Gustavo', 'gustavo_fan@hotmail.com'),
+                      (500, 'Bruna', 'bruna_costa@hotmail.com'),
+                      (507, 'Jorge', 'jorgin_gatin@hotmail.com'),]
+    return lista_usuarios
+
+def filter_users(get_users):
+    for x in get_users:
+        if x[0] > 100:
+            return filter_users(get_users(lista_usuario))
+
+print(filter_users(get_users(lista_usuario)))
+        
+
