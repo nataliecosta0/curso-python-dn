0deb5be393a1d8416cc739ecff21eb63a02feedc
Natalie Costa <natalie.dacosta@gastecnologia.com.br>
2019-03-27 16:33:15 -0300

    Incompletoaula 4

--- semana_2/exercicios_aula_4/teste_ex_7.py
+++ semana_2/exercicios_aula_4/teste_ex_7.py
@@ -2,3 +2,22 @@
 Problema #7
 Revisite os testes do problema 6 e os renomeie baseado em critérios de aceite.
 '''
+
+from unittest import TestCase
+
+def get_users():
+    return [(10, 'Matheus', 'matheus_ss@hotmail.com'),
+                      (20, 'Lucas', 'lucas_ribeiro@hotmail.com'),
+                      (140, 'Júlia', 'julia_sensacao@hotmail.com'),
+                      (235, 'Marina', 'marina_s2@hotmail.com'),
+                      (80, 'Gustavo', 'gustavo_fan@hotmail.com'),
+                      (500, 'Bruna', 'bruna_costa@hotmail.com'),
+                      (507, 'Jorge', 'jorgin_gatin@hotmail.com'),]
+
+
+def filter_users(lista_usuario):
+    return list(filter(lambda x: x[0]> 100, lista_usuario))
+
+class Test_funcao_filtrar(TestCase):
+    def test_deve_retornar_usuarios_tupla_com_id_maior_que_100(self):
+        self.assertEqual(filter_users(get_users()), [(140, 'Júlia', 'julia_sensacao@hotmail.com'), (235, 'Marina', 'marina_s2@hotmail.com'), (500, 'Bruna', 'bruna_costa@hotmail.com'), (507, 'Jorge', 'jorgin_gatin@hotmail.com')])
--- semana_2/exercicios_aula_4/teste_ex_8.py
+++ semana_2/exercicios_aula_4/teste_ex_8.py
@@ -5,3 +5,15 @@ Baseado em ATDD e no fato de que existe uma função chamada
 uma função que recebe um valor: float e nos retorna as pizzas com preço <= ao
 valor passado
 '''
+
+from unittest import TestCase
+
+def sabores_de_pizza():
+    return [('Calabresa', 40,00),
+            ('Queijo', 30,00),
+            ('Milho', 25,00 ),
+            ('Baiana', 32,00 ),
+            ('Portuguesa', 35,00 )]
+
+def menor_valor(valor):
+    return list(filter(lambda x: x[1] <= ))
