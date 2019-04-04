aa01d6865b8b6ba4a469382da99f88b0bcf2a8ba
Natalie Moreira Da Costa <natalie.dacosta@MIDGARD.LOCAL>
2019-04-04 16:47:34 -0300

    Atualizado

--- semana_4/exercicios_aula_7/Ex05.py
+++ semana_4/exercicios_aula_7/Ex05.py
@@ -5,14 +5,12 @@ Crie um arquivo de texto usando a função open com a tabuada do 10.
 (...)
 10 x 10 = 100
 '''
-#arquivo = open('alteracao_texto.txt', 'r')
-
-tabuada = []
+arquivo = open('alteracao_texto.txt', 'w')
 
 tabuada = [f'{x} x 10 = {x * 10}\n' for x in range(1, 11)]
 
-tabuada.split(' ')
+arquivo.writelines(tabuada)
 
-#arquivo.writelines(tabuada)
+arquivo = open('alteracao_texto.txt', 'r')
 
-print(tabuada)
+print(arquivo.read())
