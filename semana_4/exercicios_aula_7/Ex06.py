6e2a9d3d2f01de65764cf19de09541816b983919
Natalie Moreira Da Costa <natalie.dacosta@MIDGARD.LOCAL>
2019-04-04 16:52:52 -0300

    Atualizado

--- semana_4/exercicios_aula_7/Ex06.py
+++ semana_4/exercicios_aula_7/Ex06.py
@@ -5,10 +5,5 @@ elemento de uma lista
 [‘1 x 10 = 10’, (...), ‘10 x 10 = 100’]
 '''
 
-arquivo = open('alteracao_texto.txt', 'w')
-
-tabuada = [f'{x} x 10 = {x * 10}\n' for x in range(1, 11)]
-
-tabuada.split()
-
-arquivo.writelines(list(tabuada)
+arquivo = open('alteracao_texto.txt', 'r')
+print(arquivo.readlines())
